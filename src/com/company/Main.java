ab20765a9e0003b5132b238f0049f8335c201d05
Alex Fontan <afontanmoreira@danielcastelao.org>
2021-02-19 09:33:03 +0100

    Añadida libreria y utilizada

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,10 @@
 package com.company;
-
+import  libreria.Visualizar;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String mensaje="hola";
+        Visualizar.visualizarConsola(mensaje);
+        Visualizar.visualizarVentana(mensaje);
     }
 }
